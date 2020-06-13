217780f4e084bcae4fb94dd3dc1fb0b87645282b
RAHUL VISHWAKARMA <33103549+rahulvish1364@users.noreply.github.com>
2020-06-14 01:07:35 +0530

    Added Cards and Chart

---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import { NativeSelect, FormControl } from "@material-ui/core";
+const CountryPicker = () => {
+  return (
+    <FormControl>
+      <NativeSelect>
+        <option value="global"></option>
+      </NativeSelect>
+    </FormControl>
+  );
+};
+
+export default CountryPicker;
