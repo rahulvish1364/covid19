23d3801599e3d8692e06301b608cde4204143732
RAHUL VISHWAKARMA <33103549+rahulvish1364@users.noreply.github.com>
2020-10-18 23:44:45 +0530

    Updates

---
+++ precache-manifest.63b0340883071a3e65d61b9e918d0c36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d61be3a29544e4a93f7b5f52bf5fec2",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "0bbf436f11185c9cc6fc",
+    "url": "/covid19/static/css/main.81396f99.chunk.css"
+  },
+  {
+    "revision": "da47c9e19edd48742490",
+    "url": "/covid19/static/js/2.63da346b.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19/static/js/2.63da346b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bbf436f11185c9cc6fc",
+    "url": "/covid19/static/js/main.048b3f1f.chunk.js"
+  },
+  {
+    "revision": "3f9ea9de9b45ec0fb219",
+    "url": "/covid19/static/js/runtime-main.59095949.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19/static/media/image.d7265326.png"
+  }
+]);
